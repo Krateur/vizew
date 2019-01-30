c10dd1074d3d77e07235db084ae005d5b9baebc4
krateur <gjacksongomez@gmail.com>
2019-01-30 15:16:28 +0000

    Add files to repositories

---
+++ wp-content/themes/vizew/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function name_public () {
+    wp_enqueue_style('stylesheet', get_stylesheet_uri());
+    wp_enqueue_script('jquery', get_theme_file_uri('js/jquery/jquery-2.2.4.min.js'), NULL, '1.0', true);
+    wp_enqueue_script('bootstrap-popper', get_theme_file_uri('js/bootstrap/popper.min.js'), NULL, '1.0', true);
+    wp_enqueue_script('bootstrap', get_theme_file_uri('js/bootstrap/bootstrap.min.js'), NULL, '1.0', true);
+    wp_enqueue_script('plugin', get_theme_file_uri('js/plugins/plugins.js'), NULL, '1.0', true);
+    wp_enqueue_script('active', get_theme_file_uri('js/active.js'), NULL, '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'name_public');
